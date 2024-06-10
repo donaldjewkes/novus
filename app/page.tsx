1e37aa69fd824bfd2b65a432056f90bd327afee1
Manveer <m13basra@gmail.com>
2024-06-09 19:31:31 -0700

    Update landing copy and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
     <main className="bg-white">
       
       <div className="h-screen relative">
-        <Image className="absolute opacity-70"
+        <Image className="absolute opacity-50"
+                style={{
+                  marginTop: '64px',
+                }}
                 src="/vanbg2.svg"
                 alt="Vercel Logo"
                 layout="fill"
@@ -30,13 +33,20 @@ export default function Home() {
               </div>
               <div className="p-4 sm:p-12 w-full flex flex-col items-start sm:items-center ">
                 <div className="flex flex-col justify-right">
-                  <div className="text-3xl sm:text-7xl font-medium text-zinc-900">
-                    Community for the ambitious.
+                  <div
+                    className="text-2xl sm:text-2xl font-medium text-zinc-900"
+                    style={{
+                      maxWidth: '800px',
+                      fontSize: '32px',
+                      lineHeight: 1.4
+                    }}
+                  >
+                    We&apos;re a group of founders and creatives that meet every week to work on projects with impact and scale.
                   </div>
-                  <div  className="pt-6 w-full flex flex-col sm:flex-row items-start justify-between">
-                    <a draggable="false" href="https://lu.ma/novus">
-                      <button  type="button" className="group text-gray-900 bg-dgold transition duration-150 hover:bg-zinc-900 rounded px-4 py-2 flex flex-row items-center">
-                        <div className="text-center text-2xl font-ibm text-white">Attend</div>
+                  <div  className="pt-6 w-full flex flex-col sm:flex-row items-center justify-between">
+                    <a draggable="false" href="https://lu.ma/novus" target="_blank">
+                      <button  type="button" className="group text-gray-900 transition duration-150 hover:bg-zinc-900 rounded px-3 py-2 flex flex-row items-center" style={{ background: '#2196F3' }}>
+                        <div className="text-center text-2xl font-ibm text-white" style={{ fontSize: '20px' }}>Attend</div>
                             <Image className="group-hover:-translate-y-0.5 transition duration-150 group-hover:translate-x-0.5 ml-2 mt-0.5 fill-white" src="/arrow.svg" width={25} height={25} alt="arrow"/>
                       </button>
                     </a>
